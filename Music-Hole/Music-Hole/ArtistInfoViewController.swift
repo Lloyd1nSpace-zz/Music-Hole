3962ed5df348925eb6ae233591f5a0ca7d67ed10
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-09-11 19:27:45 -0400

    Getting there

--- Music-Hole/Music-Hole/ArtistInfoViewController.swift
+++ Music-Hole/Music-Hole/ArtistInfoViewController.swift
@@ -22,6 +22,7 @@ class ArtistInfoViewController: UIViewController, UIScrollViewDelegate {
     var discographyLabel = UILabel()
     var artistDiscographyStackView = UIStackView()
     var artistDiscographyImage = UIImageView()
+    var artistDiscographyLabel = UILabel()
     var similarArtistsLabel = UILabel()
     var similarArtistsStackView = UIStackView()
     var similarArtistImage = UIImageView()
@@ -41,14 +42,13 @@ class ArtistInfoViewController: UIViewController, UIScrollViewDelegate {
     
     func createViews() {
         
-        
-        //        self.extendedLayoutIncludesOpaqueBars = false
-        //        self.artistScrollView = UIScrollView(frame: self.view.bounds)
-        //  self.edgesForExtendedLayout = UIRectEdge.None
+        self.extendedLayoutIncludesOpaqueBars = false
+        self.artistScrollView = UIScrollView(frame: self.view.bounds)
+        self.edgesForExtendedLayout = UIRectEdge.None
         self.artistScrollView.delegate = self
-        //self.artistScrollView.autoresizingMask = UIViewAutoresizing.FlexibleHeight
-        // self.artistScrollView.userInteractionEnabled = true
-        //   self.artistScrollView.scrollEnabled = true
+        self.artistScrollView.autoresizingMask = UIViewAutoresizing.FlexibleHeight
+        self.artistScrollView.userInteractionEnabled = true
+        self.artistScrollView.scrollEnabled = true
         
         self.artistImage.layer.masksToBounds = true
         self.artistImage.layer.cornerRadius = 8
@@ -116,11 +116,11 @@ class ArtistInfoViewController: UIViewController, UIScrollViewDelegate {
     
     func viewConstraints() {
         
+        let viewsHeights = self.artistImage.frame.height + self.bioLabel.frame.height + self.artistBioTextView.frame.height + self.discographyLabel.frame.height + self.artistDiscographyStackView.frame.height + self.similarArtistsLabel.frame.height + self.similarArtistsStackView.frame.height
         
         self.artistScrollView.snp_makeConstraints { (make) in
-            //    make.top.equalTo(self.view)
             make.width.equalTo(self.view)
-            make.height.equalTo(self.view)
+            make.height.equalTo(self.view).offset(viewsHeights)
             make.centerX.equalTo(self.view)
             make.centerY.equalTo(self.view)
         }
@@ -182,12 +182,7 @@ class ArtistInfoViewController: UIViewController, UIScrollViewDelegate {
         self.similarArtistsStackView.alignment = UIStackViewAlignment.Center
         self.similarArtistsStackView.distribution = UIStackViewDistribution.EqualSpacing
         
-        //   let viewsWidths = self.artistImage.frame.width + self.bioLabel.frame.width + self.artistBioTextView.frame.width + self.discographyLabel.frame.width + self.artistDiscographyStackView.frame.width + self.similarArtistsLabel.frame.width + self.similarArtistsStackView.frame.width
-        
-        //  let viewsHeights = self.artistImage.frame.height + self.bioLabel.frame.height + self.artistBioTextView.frame.height + self.discographyLabel.frame.height + self.artistDiscographyStackView.frame.height + self.similarArtistsLabel.frame.height + self.similarArtistsStackView.frame.height
-        
-        //self.artistScrollView.contentSize.height = viewsHeights
-        
+        //  self.artistScrollView.contentSize = CGSize(width: self.view.frame.width, height: viewsHeights)
         // self.artistScrollView.contentSize = CGSize(width: viewsWidths, height: viewsHeights)
     }
     
