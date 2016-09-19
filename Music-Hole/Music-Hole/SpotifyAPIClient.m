62813c5335581923b35ba5c97a05c30b21a9af17
bmanjuu <bettyofung@gmail.com>
2016-09-19 13:02:23 -0400

    added bridging header

---
+++ Music-Hole/Music-Hole/SpotifyAPIClient.m
@@ -0,0 +1,11 @@
+//
+//  SpotifyAPIClient.m
+//  Music-Hole
+//
+//  Created by Betty Fung on 9/19/16.
+//  Copyright © 2016 Lloyd W. Sykes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Spotify/Spotify.h>
+
