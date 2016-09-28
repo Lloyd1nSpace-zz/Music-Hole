1cff6637d5a5df4d74db4964db72ce4dc6a97f28
bmanjuu <bettyofung@gmail.com>
2016-09-28 13:50:11 -0400

    adding function to obtain spotify id for artist

--- Music-Hole/Music-Hole/SpotifyAPIClient.swift
+++ Music-Hole/Music-Hole/SpotifyAPIClient.swift
@@ -15,9 +15,38 @@ class SpotifyAPIClient {
     
     class func getArtistIDWithCompletion(artistName: String, completion: @escaping (String) ->()) {
         
-        if 
+        let formattedArtistName = ArtistInfo.formatArtistName(selectedArtistName: artistName)
+        let formattedArtistForURL = formattedArtistName.replacingOccurrences(of: " ", with: "+")
         
-        let artistURLString = "https://api.spotify.com/v1/search?q=\(artistName)&type=artist"
+        let artistURLString = "https://api.spotify.com/v1/search?q=\(formattedArtistForURL)&type=artist"
+        guard let artistURL = URL(string: artistURLString) else {
+            print("could not unwrap artist url string")
+            return
+        }
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: artistURL, completionHandler: { (data, response, error) in
+            
+            if let data = data {
+                if let responseDictionary = try? JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    
+                    
+                    
+                    if let responseDictionary = responseDictionary {
+                        print("blah")
+                    } else {
+                        print("There was problem unwrapping the responseDictionary in the API Client.")
+                    }
+                } else {
+                    print("There was a problem converting JSON to NSDictionary in API Client.")
+                }
+            } else if let error = error {
+                print("There was a problem unwrapping the data in the API Client or a general networking error: \(error.localizedDescription)")
+            }
+        })
+        
+        task.resume()
+        
     }
     
     class func getArtistDiscographyWithCompletion(artistName: String, completion: @escaping (NSDictionary) ->() ) {
