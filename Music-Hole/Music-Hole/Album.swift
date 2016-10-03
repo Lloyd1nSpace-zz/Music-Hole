6cb7fe0a57bb2b1888ee1391b7fdc7fc43f653c2
bmanjuu <bettyofung@gmail.com>
2016-10-03 15:59:12 -0400

    commented out equatable protocol for custom Album objects

--- Music-Hole/Music-Hole/Album.swift
+++ Music-Hole/Music-Hole/Album.swift
@@ -8,13 +8,13 @@
 
 import Foundation
 
-class Album: Hashable, Equatable {
+class Album {
     
     let albumArtist : String //not sure if we need this if this is required in API parameter
     let albumName : String
     let albumImage : UIImage
     
-    var hashValue: Int { get { return albumName.hashValue } }
+    // var hashValue: Int { get { return albumName.hashValue } }
     
     
     init(albumArtist: String, albumName: String, albumImage: UIImage) {
@@ -24,20 +24,20 @@ class Album: Hashable, Equatable {
     }
 }
 
-extension Array where Element : Equatable {
-    
-    var unique: [Element] {
-        var uniqueValues: [Element] = []
-        forEach { item in
-            if !uniqueValues.contains(item) {
-                uniqueValues += [item]
-            }
-        }
-        return uniqueValues
-    }
-    
-}
-
-func ==(lhs: Album, rhs: Album) -> Bool {
-    return lhs.albumName == rhs.albumName
-}
+//extension Array where Element : Equatable {
+//    
+//    var unique: [Element] {
+//        var uniqueValues: [Element] = []
+//        forEach { item in
+//            if !uniqueValues.contains(item) {
+//                uniqueValues += [item]
+//            }
+//        }
+//        return uniqueValues
+//    }
+//    
+//}
+//
+//func ==(lhs: Album, rhs: Album) -> Bool {
+//    return lhs.albumName == rhs.albumName
+//}
