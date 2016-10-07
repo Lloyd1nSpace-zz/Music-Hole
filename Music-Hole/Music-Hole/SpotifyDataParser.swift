c6eea62fe5e63d15327e87c0517b14de4b00ae43
bmanjuu <bettyofung@gmail.com>
2016-10-07 14:54:08 -0400

    starting to restructure spotify api client

---
+++ Music-Hole/Music-Hole/SpotifyDataParser.swift
@@ -0,0 +1,13 @@
+//
+//  SpotifyAPIClientDataParser.swift
+//  Music-Hole
+//
+//  Created by Betty Fung on 10/7/16.
+//  Copyright © 2016 Lloyd W. Sykes. All rights reserved.
+//
+
+import Foundation
+
+struct spotifyDataParser {
+    
+}
